98a8c6ba124e457b6cd79eb0727c7f44a1dfbebe
ddjp <sandyford210489@gmail.com>
2023-03-23 23:41:47 +0000

    Test fix

--- src/components/__tests__/App.test.tsx
+++ src/components/__tests__/App.test.tsx
@@ -1,13 +1,12 @@
 import { checkAccessibility, customRender } from '@test-utils'
-import { screen } from '@testing-library/react'
 import App from '../App'
 
 describe('App', () => {
   checkAccessibility(<App />)
 
-  test('Renders main page correctly', async () => {
-    customRender(<App />)
-    const buttonCount = await screen.findByRole('button')
-    expect(buttonCount.innerHTML).toBe('count is 0')
+  test('Renders main page correctly', () => {
+    const { getByText } = customRender(<App />)
+    expect(getByText(/Header/i)).toBeInTheDocument()
+    expect(getByText(/Hello World!/i)).toBeInTheDocument()
   })
 })
