94cf8fd4d1a776fa50cd3de79b11f743b56af75a
ddjp <sandyford210489@gmail.com>
2023-03-23 23:34:11 +0000

    Update wrapper.tsx

--- src/test-helper/wrapper.tsx
+++ src/test-helper/wrapper.tsx
@@ -1,13 +1,42 @@
-import { type ComponentType, type ReactElement, type ReactNode } from 'react'
-import * as RTL from '@testing-library/react'
+import React from 'react'
+import {
+  render,
+  type RenderResult,
+  type RenderOptions,
+} from '@testing-library/react'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-const AllTheProviders = ({ children }: { children: ReactNode }): ReactNode => {
-  return <>{children}</>
+interface IExtendedRenderOptions extends RenderOptions {
+  withRouter?: boolean
 }
-const customRender = (
-  ui: ReactElement,
-  options?: Omit<RTL.RenderOptions, 'queries'>
-): RTL.RenderResult =>
-  RTL.render(ui, { wrapper: AllTheProviders as ComponentType, ...options })
 
-export const customRTL = { ...RTL, render: customRender }
+const wrapInRouter = (componentTree: JSX.Element): JSX.Element => (
+  <Router>{componentTree}</Router>
+)
+
+const setupComponent = (
+  ui: JSX.Element,
+  renderOptions?: IExtendedRenderOptions
+): JSX.Element => {
+  if (renderOptions == null) return ui
+  let componentTree = <>{ui}</>
+  if (renderOptions.withRouter ?? false)
+    componentTree = wrapInRouter(componentTree)
+  return componentTree
+}
+
+const customRender = (
+  ui: JSX.Element,
+  renderOptions?: IExtendedRenderOptions
+): RenderResult => {
+  try {
+    const componentTree = setupComponent(ui, renderOptions)
+    return render(componentTree)
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export * from '@testing-library/react'
+export { customRender, type IExtendedRenderOptions }
